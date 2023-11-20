0d62b2c8f91731a669be9b9a9f95757b725aaaee
Akb-25 <25bakshay2004@gmail.com>
2023-11-20 20:41:26 +0530

    starting from scratch base 1

---
+++ MergedSortedArray88.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self,nums1:list[int],m:int,nums2:list[int],n:int):
+        last=m+n-1
+        m-=1
+        n-=1
+        while n>=0:
+            if m>=0 and nums1[m]>nums2[n]:
+                nums1[last]=nums1[m]
+                m-=1
+            else:
+                nums1[last]=nums2[n]
+                n-=1
+            last-=1
+        return nums1
