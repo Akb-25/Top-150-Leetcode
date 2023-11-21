901c3a6b31b26bf914dbe7d85f4cd609e024de10
Akb-25 <25bakshay2004@gmail.com>
2023-11-21 13:49:19 +0530

    do more

--- BesTimeToSellAStock.py
+++ BesTimeToSellAStock.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min=float("inf")
-        minKey=-1
-        maxKey=-1
         max=0
-        for key,val in prices:
-            if val<min and key>minKey:
-                min=val
-                minKey=key
-            if val>max and key>maxKey and key>minKey:
-                max=val
-                maxKey=key
-        return max-min
+        for i in prices:
+            if i<min:
+                min=i
+            elif i-min>max:
+                max=i-min
+        return max
--- temp.py
+++ temp.py
@@ -1,23 +1,15 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        index=0
-        j=0
-        nums[index]=nums[0]
-        index+=1
-        for i in range(1,len(nums)):
-            if nums[i]==nums[i-1]: 
-                j+=1
-                if j<2:
-                    nums[index]=nums[i] max,min=0,float("inf")
-        for cash in prices:
-            if cash<min:
-                min=cash
-            elif cash-min>max:
-                max=cash-min
-        return max
-                    index+=1
-            if nums[i]!=nums[i-1]:
-                nums[index]=nums[i]
-                index+=1
-                j=0
-        return index
+profit=0
+        max=0
+        buy=0
+        min=float("inf")
+        area=len(prices)
+        for i in range(area):
+            if prices[i]<min:
+                min=prices[i]
+            if i!=area-1:
+                if prices[i]>prices[i+1]:
+                    profit+=prices[i]-min
+                    min=float("inf")
+            if i == area-1:
+                profit+=prices[i]-min
+        return profit
